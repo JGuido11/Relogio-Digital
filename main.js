ddee8cbf0e504c067271541e4989e65e67224bc5
JGuido11 <82364288+JGuido11@users.noreply.github.com>
2022-05-06 03:07:08 -0300

    Clock work

---
+++ main.js
@@ -0,0 +1,19 @@
+// let hours = document.getElementById('hours');
+// let minutes = document.getElementById('minutes');
+// let seconds = document.getElementById('seconds');
+// let AmPm = document.getElementById('AmPm');
+
+// let h = new Date().getHours();
+// let m = new Date().getMinutes();
+// let s = new Date().getSeconds();
+// let am = h >= 12 ? "PM" : "AM";
+
+// //converte relogio de 24hrs para 12hrs
+// if( h > 12){
+//   h = h - 12;
+// }
+
+// hours.innerHTML = h;
+// minutes.innerHTML = m;
+// seconds.innerHTML = s;
+// AmPm.innerHTML = am;
